9d12df1db3688936ffc6ea5e1ebc7f9cac43800f
tim <tyuen003@gmail.com>
2024-07-19 07:06:18 -0400

    welcome changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Welcome to Tim's first deployed fullstack app on AWS cloud using Amplify!!!
         </p>
         <a
           className="App-link"
